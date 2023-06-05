0bccc5e1d0158ec0b2b386ddab44d053e674a721
Alexandra <dom.levchenko.l7@gmail.com>
2023-06-05 14:07:09 +0500

    add pages component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,14 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppProps = {
+  genres: string;
+  filmDates: string;
+
+}
+function App({genres, filmDates}: AppProps): JSX.Element {
+  return (
+    <MainPage genres={genres} filmDates={filmDates} />
+  );
 }
 
 export default App;
