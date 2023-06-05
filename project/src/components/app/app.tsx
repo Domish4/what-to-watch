4cbe8f6b94c143d1f5423c178bc3f32522c85b81
Alexandra <dom.levchenko.l7@gmail.com>
2023-06-05 15:44:41 +0500

    add routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import SignInPage from '../../pages/sign-in-page/sign-in-page';
+import MyListPage from '../../pages/my-list-page/my-list-page';
+import MoviePage from '../../pages/movie-page/movie-page';
+import AddReviewPage from '../../pages/add-review-page/add-review-page';
+import PlayerPage from '../../pages/player-page/player-page';
+import ErrorPage from '../../pages/error-page/error-page';
+import PrivateRoute from '../private-routes/private-routes';
 
 type AppProps = {
   genres: string;
@@ -7,7 +15,22 @@ type AppProps = {
 }
 function App({genres, filmDates}: AppProps): JSX.Element {
   return (
-    <MainPage genres={genres} filmDates={filmDates} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainPage genres={genres} filmDates={filmDates} />} />
+        <Route path='/login' element={<SignInPage />} />
+        <Route path='/mylist' element={
+          <PrivateRoute>
+            <MyListPage />
+          </PrivateRoute>
+        }
+        />
+        <Route path='/films/:id' element={<MoviePage />} />
+        <Route path='/films/:id/review' element={<AddReviewPage />} />
+        <Route path='/player/:id' element={<PlayerPage />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/pages/error-page/error-page.tsx
@@ -0,0 +1,7 @@
+function ErrorPage(): JSX.Element {
+  return (
+    <div><h1>404. Такая страница не существует</h1></div>
+  );
+}
+
+export default ErrorPage;
